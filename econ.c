85cd7dc070a7144373b75e81992bafb15eb1ba57
J Chen <chenjhr8899@gmail.com>
2020-03-26 18:19:22 -0500

    first commit

--- econ.c
+++ econ.c
@@ -2,8 +2,22 @@
 #include <string.h>
 
 int main() {
+    
+    double prevInfRate;
+    double curInfRate;
 
-    printf("The economy is great!");
+    printf("Please enter the following information to determine the state of the economy\nPrevious month's posted inflation rate: ");
+    scanf("%lf", &prevInfRate);
+    printf("This month's posted inflation rate: ");
+    scanf("%lf", &curInfRate);
+    if((prevInfRate - curInfRate) > .5){
+        printf("The economy need a little improving. Maybe look towards investing less and spending more.");
+    }
+    else{
+        printf("It's not that bad");
+    }
+
+
     return 0;
 
 }
