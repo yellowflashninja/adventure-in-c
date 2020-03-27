570ba8db021ad3ef738c276101b6e8b23fcd4ae6
J Chen <chenjhr8899@gmail.com>
2020-03-26 23:48:04 -0500

    fifth commit

--- econ.c
+++ econ.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main() {
-    
+const double nairuInf = 2.4;
+const double nairuUnemp = 3.5;
+
+int inflation(){
     double prevInfRate;
     double curInfRate;
 
@@ -11,13 +14,59 @@ int main() {
     printf("This month's posted inflation rate: ");
     scanf("%lf", &curInfRate);
     if((prevInfRate - curInfRate) > .5){
-        printf("The economy need a little improving. Maybe look towards investing less and spending more.");
+        return 2;
+    }
+    else if((prevInfRate - curInfRate) < -.5){
+        return 0;
     }
     else{
-        printf("It's not that bad");
+        return 1;
     }
+}
 
 
+int unemployment(){
+    double prevUnemRate;
+    double curUnemRate;
+
+    printf("Previous month's posted unemployment rate: ");
+    scanf("%lf", &prevUnemRate);
+    printf("This month's posted unemployment rate: ");
+    scanf("%lf", &curUnemRate);
+    if((prevUnemRate - curUnemRate) > .5){
+        return 2;
+    }
+    else if((prevUnemRate - curUnemRate) < -.5){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+void compare(){
+
+    int unemployment();
+    int inflation();
+
+    int y = inflation();
+    int x = unemployment();
+
+    if(x + y == 4){
+        printf("Yikes economy not doing too hot");
+    }
+    else if(x + y >= 3){
+        printf("Economy is ok");
+    }
+   /* else{
+        printf("Let's go! The economy is doing fairly well.");
+    }
+    */
+}
+
+int main() {
+    
+    compare();    
     return 0;
 
 }
