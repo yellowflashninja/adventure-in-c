1d1d756306abc65260594fb38e9a301e33f46e7c
J Chen <chenjhr8899@gmail.com>
2020-04-06 15:10:33 -0500

    7

---
+++ Lesson 1/minMaxAvg.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+    double x, min, max, sum, avg;
+    if(scanf("%lf",&x) != 1){
+        printf("No data found - bye!\n");
+        exit(1);
+    }
+    min = max = sum = avg = x;
+    printf("%5s%9s%9s%9s%12s%12s\n%5s%9s%9s%9s%12s%12s\n\n",
+        "Count", "Item", "Min", "Max", "Sum", "Average",
+        "-----", "----", "---", "---", "---", "-------");
+    printf("%5d%9.1f%9.1f%12.3f%12.3f\n", 1, x, min, max, sum, avg);
+    for(i=2; scanf("%lf", &x) == 1; ++i){
+        if(x < min){
+            min = x;
+            printf("%d",min);
+        }
+        else if(x > max){
+            max = x;
+        }
+        sum += x;
+        avg = sum/i;
+        printf("%5d%9.1f%9.1f%9.1f%12.3f%12.3f\n", i, x, min, max, sum, avg);
+    }
+    return 0;
+}
